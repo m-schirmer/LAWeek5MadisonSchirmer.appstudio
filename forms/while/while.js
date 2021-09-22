962e8bc04f3f99a6413fa34c22292a0b1605788e
Madison Schirmer <90575585+m-schirmer@users.noreply.github.com>
2021-09-22 16:21:23 -0500

    for (names array)

---
+++ forms/while/while.js
@@ -0,0 +1,10 @@
+// First Program
+/*
+let wantToContinue = true
+let i = 1
+while(wantToContinue){
+  console.log(`This loop is iteration ${i}.`)
+  wantToContinue = confirm("Do you want to continue?")
+  i++
+}
+*/
